737f41e4517bbc9ad1ba5df77561f164a00416cb
Mahdi <mahdi1nap@gmail.com>
2026-06-10 12:03:32 +0330

    create services folder

--- src/components/templates/homePage.jsx
+++ src/components/templates/homePage.jsx
@@ -1,11 +1,16 @@
 import { useEffect, useState } from "react"
 import TablesCoins from "../modules/tablesCoins"
+import { getCoinList } from "../services/cryptoAPI"
 
 export default function HomePage(){
     const [coins,setCoins]=useState([])
     useEffect(()=>{
-        const key="CG-ns88QYcsnZPypRv1cX61yUDu"
-        fetch (`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&per_page=20&page=1&x_cg_demo_api_key=${key}`).then(res=>res.json()).then(data=>setCoins(data))
+        const getData= async ()=>{
+            const res=await fetch (getCoinList())
+            const json=await res.json()
+            setCoins(json)
+        }
+        getData()
     
     },[])
     return(
