fcd2199e3dd9c5f1d73f4843c05b9b60d5c0239d
Mahdi <mahdi1nap@gmail.com>
2026-06-16 13:35:57 +0330

    fix: handle null values from API

--- src/components/modules/tablesCoins.jsx
+++ src/components/modules/tablesCoins.jsx
@@ -4,7 +4,7 @@ import { RotatingLines } from "react-loader-spinner";
 import styles from "./tableCoins.module.css"
 
 export default function TablesCoins({coins,loader}){
-
+   
     console.log(coins);
     
     return(
@@ -13,17 +13,17 @@ export default function TablesCoins({coins,loader}){
         <table className={styles.table}>
             <thead >
             <tr>
-                <th>Conin</th>
+                <th>Coin</th>
                 <th>Name</th>
                 <th>Price</th>
                 <th>24h</th>
-                <th>Total Volum</th>
+                <th>Total Volume</th>
                 <th>Chart</th>
             </tr></thead>
             <tbody>
                 
                     {coins.map((coin)=>(
-                        <TableRow coin={coin}/>
+                        <TableRow key={coin.id} coin={coin}/>
                     )       
                     )}
                 
@@ -34,13 +34,27 @@ export default function TablesCoins({coins,loader}){
     )
 }
 const TableRow=({coin})=>{
+     const displayStrings=(value)=>{
+        if (value===null || value ===undefined || value ===""){
+            return "-"
+        }
+        return value
+    }
+    const displayNumbersToLocaleString=(value)=>{
+       return value !=null ? value.toLocaleString():"-"
+
+    }
+    const displayNumbersToFixed=(value)=>{
+        return value !=null ?value.toFixed(2):"-"
+    }
+   
     return(
-        <tr key={coin.id}>
-            <td><div className={styles.symbol}><img src={coin.image}/><span>{coin.symbol.toUpperCase()}</span></div></td>
-            <td>{coin.name}</td>
-            <td>{coin.current_price.toLocaleString()}</td>
-            <td className={coin.price_change_percentage_24h>0?styles.green:styles.red}>{coin.price_change_percentage_24h.toFixed(2)}%</td>
-            <td>{coin.total_volume.toLocaleString()}</td>  
+        <tr >
+            <td><div className={styles.symbol}><img src={coin.image}/><span>{coin.symbol?coin.symbol.toUpperCase():"-"}</span></div></td>
+            <td>{displayStrings(coin.name)}</td>
+            <td>{displayNumbersToLocaleString(coin.current_price)}</td>
+            <td className={coin.price_change_percentage_24h>0?styles.green:styles.red}>{displayNumbersToFixed(coin.price_change_percentage_24h)}</td>
+            <td>{displayNumbersToLocaleString(coin.total_volume) }</td>  
             <td><img src={coin.price_change_percentage_24h > 0 ? ChartUp :ChartDown}/></td>
             </tr>
     )
