e9a772ed7fbad344b941a8d64ea04ec35e028505
Mahdi <mahdi1nap@gmail.com>
2026-06-23 19:51:43 +0330

    feat:complete footer styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
 import HomePage from "./components/templates/homePage"
+import Footer from "./components/footer/footer"
 
 function App() {
   
   return (
     <>
   <HomePage/>
+  <Footer/>
     </>
   )
 }
--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,71 @@
+import styles from "./footer.module.css"
 export default function Footer(){
     return(
         <>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+        
+        <div className={styles.container}>
+        <div>
+            <h4>Products</h4>
+            <ul>
+                <li><a href="">What's New</a></li>
+                <li><a href="">Design</a></li>
+                <li><a href="">Collaboration</a></li>
+                <li><a href="">Protyping</a></li>
+                <li><a href="">Developer handoff</a></li>
+                <li><a href="">All Features</a></li>
+            </ul>
+        </div>
+        <div>
+            <h4>Products</h4>
+            <ul>
+                <li><a href="">What's New</a></li>
+                <li><a href="">Design</a></li>
+                <li><a href="">Collaboration</a></li>
+                <li><a href="">Protyping</a></li>
+                <li><a href="">Developer handoff</a></li>
+                <li><a href="">All Features</a></li>
+            </ul>
+        </div>
+        <div>
+            <h4>Products</h4>
+            <ul>
+                <li><a href="">What's New</a></li>
+                <li><a href="">Design</a></li>
+                <li><a href="">Collaboration</a></li>
+                <li><a href="">Protyping</a></li>
+                <li><a href="">Developer handoff</a></li>
+                <li><a href="">All Features</a></li>
+            </ul>
+        </div>
+        <div>
+            <h4>Products</h4>
+            <ul>
+                <li><a href="">What's New</a></li>
+                <li><a href="">Design</a></li>
+                <li><a href="">Collaboration</a></li>
+                <li><a href="">Protyping</a></li>
+                <li><a href="">Developer handoff</a></li>
+                <li><a href="">All Features</a></li>
+            </ul>
+        </div>
+        </div>
+        <hr />
+        <div className={styles.downfooter}>
+
+        <div className={styles.footerNote}>
+            <h3>Never miss on News</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus aspernatur quisquam cumque consequuntur fugit fugiat magnam omnis eveniet provident eos rerum autem, nihil recusandae quas sapiente. Similique, sed asperiores cumque a reprehenderit sit consectetur ipsam.</p>
+        </div>
+
+        <div className={styles.emailBox}>
+            <input className={styles.emailInput} type="text" placeholder="example@gmail.com" />
+            <button>Join</button>
+            <div className={styles.checkBox}>
+                <input type="checkbox"/>
+                <span>I agree to receive E-mail</span>
+            </div>
+        </div>
+        </div>
         </>
     )
 }
