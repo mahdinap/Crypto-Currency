e6e54207cd6ccfd49c9a9ecc25aeba5897659244
Mahdi <mahdi1nap@gmail.com>
2026-06-24 21:08:54 +0330

    create header structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
 import HomePage from "./components/templates/homePage"
 import Footer from "./components/footer/footer"
+import Header from "./components/header/header"
 
 function App() {
   
   return (
     <>
+    <Header/>
   <HomePage/>
   <Footer/>
     </>
