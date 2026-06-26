00e9a01c20336eecbfa5f2882636e485d191b822
Mahdi <mahdi1nap@gmail.com>
2026-06-26 13:30:03 +0330

    feat:complete header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,23 @@
+import styles from "./header.module.css"
 export default function Header(){
     return(
         <>
+        <div className={styles.container} >
+        <a href=""><img src="/src/assets/5341451.png" alt="logo" className={styles.logo}/></a>
+
         <div>
-        header
+            <ul className={styles.list}>
+                <li><a href="">Api</a></li>
+                <li><a href="">Contact us</a></li>
+                <li><a href="">Licenses</a></li>
+                <li><a href="">About us</a></li>
+            </ul>
+        </div>
+        <div className={styles.btn}>
+            <button className={styles.btn1}>btn1</button>
+            <button className={styles.btn2}>btn2</button>
+        </div>
+        
         </div>
         </>
     )
